f1f33b6e0d81f70c9f5a60a07ea0bf92d6042382
AceCoderr <shikareakash1111@gmail.com>
2025-04-10 15:40:48 +0100

    Added room creation, websocket session and room actor

--- src/main/java/Actors/RoomActor.java
+++ src/main/java/Actors/RoomActor.java
@@ -1,9 +1,29 @@
 package Actors;
 
 import akka.actor.AbstractActor;
+import akka.actor.Actor;
+import akka.actor.ActorRef;
 import akka.actor.Props;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class RoomActor extends AbstractActor {
+
+    private final String roomName;
+    private final Set<ActorRef> users = new HashSet<>();
+
+    public RoomActor(String roomName){
+        this.roomName = roomName;
+    }
+
+    public static class Join{
+        public final ActorRef user;
+        public Join(ActorRef user)
+        {
+            this.user = user;
+        }
+    }
 
     @Override
     public Receive createReceive() {
@@ -11,7 +31,6 @@ public class RoomActor extends AbstractActor {
     }
 
     public static Props props(String name){
-
         return Props.create(RoomActor.class);
     }
 }
--- src/main/java/Actors/RoomManagerActor.java
+++ src/main/java/Actors/RoomManagerActor.java
@@ -1,24 +1,24 @@
 package Actors;
 
-import akka.actor.AbstractActor;
-import akka.actor.ActorRef;
-import akka.actor.ActorSystem;
-import akka.actor.Props;
+import akka.actor.*;
 
 import akka.japi.pf.ReceiveBuilder;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-import org.springframework.stereotype.Service;
-
-
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
-
-@Service
 public class RoomManagerActor extends AbstractActor {
+
     public static class Create{}
+    public static class JoinRoom{
+        public final String roomName;
+        public final ActorRef user;
+        public JoinRoom(String roomName, ActorRef user)
+        {
+            this.roomName = roomName;
+            this.user = user;
+        }
+    }
     public static class RoomCreated
     {
         private final String roomName;
@@ -30,27 +30,31 @@ public class RoomManagerActor extends AbstractActor {
         }
     }
 
-    private ActorSystem actorSystem = ActorSystem.create("ChatSystem");
-    public ActorRef server = actorSystem.actorOf(RoomManagerActor.props(),"chatserver");
-    ObjectMapper objectMapper = new ObjectMapper();
-
     private final ConcurrentHashMap<String,ActorRef> rooms = new ConcurrentHashMap<>();
 
+    public static Props props(){
+        return Props.create(RoomManagerActor.class);
+    }
 
 
     @Override
     public Receive createReceive() {
-        return ReceiveBuilder()
+        return ReceiveBuilder.create()
                 .match(Create.class,msg -> {
                     String name = UUID.randomUUID().toString();
                     ActorRef roomRef = getContext().actorOf(RoomActor.props(name), "room-" + name);
                     getContext().watch(roomRef);
                     rooms.put(name, roomRef);
                     getSender().tell(new RoomCreated(name), getSelf());
-                }).build();
-    }
-
-    public static Props props(){
-        return Props.create(RoomManagerActor.class);
+                })
+                .match(JoinRoom.class,msg->{
+                    if (!rooms.containsKey(msg.roomName)){
+                        msg.user.tell(new Status.Failure(new Exception("room does not exists")),getSelf());
+                    }
+                    else {
+                        rooms.get(msg.roomName).forward(new RoomActor.Join(msg.user),getContext());
+                    }
+                })
+                .build();
     }
 }
